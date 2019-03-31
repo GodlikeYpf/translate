04a63d495432a5db1796042f397283f9cb33b477
GodlikeYpf <45812383+GodlikeYpf@users.noreply.github.com>
2019-03-31 20:47:17 +0800

    Add files via upload

---
+++ content.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+__author__ = 'LENOVO'
+import hashlib
+import urllib.request
+import json
+import random
+def query_content(q):
+    appid = '20190314000277133'
+    secretKey = 'BoLJPqcbnwXTGJJvy_Nc'
+    httpClient = None
+    myurl = 'http://api.fanyi.baidu.com/api/trans/vip/translate'
+    fromLang = 'zh'
+    toLang = 'en'
+    salt = random.randint(32768, 65536)
+    sign = appid + q + str(salt) + secretKey
+    m1 = hashlib.md5()
+    m1.update(sign.encode(encoding='utf-8'))
+    sign = m1.hexdigest()
+    myurl = myurl + '?appid=' + appid + '&q=' + urllib.parse.quote(
+        q) + '&from=' + fromLang + '&to=' + toLang + '&salt=' + str(salt) + '&sign=' + sign
+    response = urllib.request.urlopen(myurl).read().decode('utf8')
+    getJson = json.loads(response)
+    getInfo = getJson['trans_result']
+    s = getInfo[0]
+    re = s['dst']
+    return re
---
+++ trans.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+__author__ = 'LENOVO'
+import sys
+from PyQt5.QtWidgets import QWidget, QApplication, qApp
+from PyQt5.QtCore import Qt
+from Ui_trans import Ui_Form
+from content import *
+
+
+class Weather(QWidget, Ui_Form):
+    def __init__(self, parent=None):
+        # 继承主窗口类
+        super(Weather, self).__init__(parent)
+        self.setupUi(self)
+        self.initUi()
+
+    def initUi(self):
+        # 将 textEdit 设置为只读模式
+        self.textEdit.setReadOnly(True)
+        # 将鼠标焦点放在 lineEdit 编辑栏里
+        self.lineEdit.setFocus()
+
+    def queryWeather(self):
+        # 获取 lineEdit 中的文本
+        city = self.lineEdit.text()
+        info = query_content(city)
+        self.lineEdit.setFocus()
+        # 设置文本
+        self.textEdit.setText(info)
+
+    def keyPressEvent(self, e):
+        # 设置快捷键
+        if e.key() == Qt.Key_Return:
+            self.queryWeather()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    weather = Weather()
+    weather.show()
+    sys.exit(app.exec_())
